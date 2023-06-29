4a538fff8ee4e8f45e350a9c4a4fcf74b68afd2b
zhusen <320073580@qq.com>
2023-06-29 13:53:43 +0800

    's'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,14 @@
 import Layout from "../components/myLayout";
+import React from 'react'
 
-export default ()  =>(
-    <Layout title={"首页"}>
-        <h1>Index</h1>
-    </Layout>
+const Homepage = () => (
+  <div>
+    <h1>Google</h1> 
+    <iframe src="https://www.google.com/" width="100%" height="800" />
+    
+    <h1>chat</h1> 
+    <iframe src="https://chat.openai.com/" width="100%" height="800" />
+  </div>
 )
+
+export default Homepage 
